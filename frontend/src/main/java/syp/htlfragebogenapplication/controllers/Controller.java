095cbe0f0bbb6e0ce9b88ffbeb1d657bf43c84dd
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2025-03-18 20:54:06 +0100

    feat(MainView.fxml): added search bar in frontend

--- frontend/src/main/java/syp/htlfragebogenapplication/controllers/Controller.java
+++ frontend/src/main/java/syp/htlfragebogenapplication/controllers/Controller.java
@@ -11,12 +11,5 @@ public class Controller {
     @FXML
     private Button myButton;
 
-    @FXML
-    public void handleButtonClick() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Button Clicked");
-        alert.setHeaderText(null);
-        alert.setContentText("You clicked the button!");
-        alert.showAndWait();
-    }
+
 }
