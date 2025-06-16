9beb209e8d6566708d9fb5d8b6625f353ade15fc
melanie-bauer <me.bauer2008@gmail.com>
2025-06-16 19:20:29 +0200

    feat: rename folder for frontend evaluation application correct #APM-47

--- frontend-evaluation-app/src/main/java/module-info.java
+++ frontend-evaluation-app/src/main/java/module-info.java
@@ -1,8 +1,11 @@
-module at.java.frontendevaluationapp {
+module at.java.frontendevluationapp {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
 
-    opens at.java.frontendevaluationapp to javafx.fxml;
-    exports at.java.frontendevaluationapp;
+    exports at.java.frontendevluationapp.model to com.fasterxml.jackson.databind;
+    opens at.java.frontendevluationapp to javafx.fxml;
+    exports at.java.frontendevluationapp;
 }
