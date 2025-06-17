d6c6bf8949d2a285dc31390e0826041c10a4da96
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2025-06-17 15:15:55 +0200

    feat: streamline build process by adding dedicated script for evaluation application FBH-51

---
+++ scripts/build-evaluation.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e 
+
+cd frontend-evaluation-app
+
+
+mvn clean package
+rm -rf original-frontend-evaluation-app-1.0-SNAPSHOT.jar
+mv ./target/frontend-evaluation-app-1.0-SNAPSHOT.jar ./target/fragebogen-evaluation.jar
+
+
